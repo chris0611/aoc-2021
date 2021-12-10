09556aec69fe02680587dfb5284fdd0e65e0e505
chris0611 <christopher.olberg@hotmail.com>
2021-12-10 12:55:43 +0100

    Solved Day 10 Part B

--- crates/day10/src/main.rs
+++ crates/day10/src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
+
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     println!("{}", day10a(INPUT));
+    println!("{}", day10b(INPUT));
 }
 
 fn day10a(input: &str) -> usize {
@@ -31,13 +34,59 @@ fn day10a(input: &str) -> usize {
                         scores[3] += 25137;
                     }
                 }
-                b => chunks.push(b), 
+                b => chunks.push(b),
             }
         }
         chunks.clear();
     });
 
     scores.iter().sum()
+}
+
+fn day10b(input: &str) -> usize {
+    let mut chunks: Vec<char> = Vec::new();
+    let mut scores: Vec<usize> = Vec::new();
+
+    let matching = HashMap::from([(')', '('), (']', '['), ('}', '{'), ('>', '<')]);
+
+    let scoring = HashMap::from([('(', 1), ('[', 2), ('{', 3), ('<', 4)]);
+
+    input.lines().for_each(|line| {
+        let mut corrupt = false;
+
+        for c in line.chars() {
+            match c {
+                ')' | ']' | '}' | '>' => {
+                    if chunks.pop().unwrap() != *matching.get(&c).unwrap() {
+                        corrupt = true;
+                        break;
+                    }
+                }
+                b => chunks.push(b),
+            }
+        }
+
+        if !corrupt {
+            let mut score = 0;
+
+            while !chunks.is_empty() {
+                let b = chunks.pop().unwrap();
+                match b {
+                    '(' | '[' | '{' | '<' => {
+                        score *= 5;
+                        score += scoring.get(&b).unwrap();
+                    }
+                    _ => (),
+                }
+            }
+            scores.push(score);
+        }
+
+        chunks.clear();
+    });
+
+    scores.sort_unstable();
+    scores[scores.len() / 2]
 }
 
 #[cfg(test)]
@@ -49,6 +98,14 @@ mod tests {
     fn part_a() {
         let expected = 26397;
         let actual = day10a(TEST_INPUT);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn part_b() {
+        let expected = 288957;
+        let actual = day10b(TEST_INPUT);
 
         assert_eq!(expected, actual);
     }
